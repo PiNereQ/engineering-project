23c6702c6b1a8e97aa089b241c9701437864ee79
vYgnette13 <798rar@gmail.com>
2025-06-02 11:50:10 +0200

    Increased amount of loaded coupons in CouponListBloc and added mock coupon functions to DebugScreen

--- lib/presentation/screens/debug_screen.dart
+++ lib/presentation/screens/debug_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 import 'package:proj_inz/presentation/widgets/input/buttons/custom_text_button.dart';
 
 class DebugScreen extends StatefulWidget {
@@ -13,11 +16,55 @@ class _DebugScreenState extends State<DebugScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Debug'),),
-      body: Column(
+      body: const Column(
         children: [
+          CustomTextButton(
+            label: '_addMockCoupons',
+            onTap: _addMockCoupons
+          ),
+          CustomTextButton(
+            label: '_deleteMockCoupons',
+            onTap: _deleteMockCoupons
+          ),
         ],
       )
     );
     
   }
+}
+
+void _addMockCoupons() async {
+  final collection = FirebaseFirestore.instance
+        .collection('coupons');
+  
+  for (var i=0;i<200;i++) {
+    await collection.add({
+      'code': '000',
+      'createdAt': FieldValue.serverTimestamp(),
+      'description': 'test',
+      'expiryDate': Timestamp.fromDate(DateTime.now().add(const Duration(days: 30))),
+      'hasLimits': false,
+      'isSold': false,
+      'pricePLN': i+1,
+      'reduction': i+1,
+      'reductionIsPercentage': true,
+      'sellerId': '0',
+      'shopId': '0',
+      'worksInStore': true,
+      'worksOnline': false
+    });
+  }
+}
+
+void _deleteMockCoupons() {
+  final collection = FirebaseFirestore.instance
+        .collection('coupons');
+
+  collection.get().then((snapshot) async {
+    for (var doc in snapshot.docs) {
+      if (doc.id != '0') {
+        await doc.reference.delete();
+      }
+    }
+  });
 }
