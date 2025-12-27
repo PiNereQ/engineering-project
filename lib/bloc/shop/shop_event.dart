8508a9a922112e929184e61ba8589faf34d19bff
vYgnette13 <798rar@gmail.com>
2025-12-27 22:44:42 +0100

    Refactored AddScreen to use a search popup instead of dropdown list for coupon selection [SCRUM-197]

--- lib/bloc/shop/shop_event.dart
+++ lib/bloc/shop/shop_event.dart
@@ -7,4 +7,12 @@ abstract class ShopEvent extends Equatable {
   List<Object> get props => [];
 }
 
+class SearchShopsByName extends ShopEvent {
+  final String query;
+  const SearchShopsByName(this.query);
+
+  @override
+  List<Object> get props => [query];
+}
+
 class LoadShops extends ShopEvent {}
